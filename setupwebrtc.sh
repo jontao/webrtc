ce933d2bfcb758453d3eda407452737d68fe3a30
jonta <jonta@jonta.cn>
2018-07-31 10:19:15 -0400

    upload

---
+++ setupwebrtc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#203.208.40.111  dl.google.com
+#203.208.41.63  developers.google.com
+
+
+export PATH=$PATH:/home/jonta/webrtc/google_appengine
+
+dev_appserver.py   /home/jonta/webrtc/apprtc/out/app_engine --skip_sdk_update_check --host=192.168.1.124 &
+
+/home/jonta/webrtc/colider/bin/collidermain -port=8089 -tls=false &
+
+service coturn start
+nginx
+service php7.0-fpm restart
